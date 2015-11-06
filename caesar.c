5a0060ce55577d3af3e7a0c6ffb0a83c262b3b77
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-06 10:46:06 +0100

    print number of command line arguments

--- caesar.c
+++ caesar.c
@@ -7,4 +7,8 @@ int main (int argc, string argv[])
 {
     // print hello world (test)
     printf("hello, world\n");
+    
+    // print number of command line arguments
+    printf("Argc: %i\n", argc);
+    
 }
